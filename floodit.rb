e334041675d57c3c7efb3c3dbf4c387abc0a8e78
Joseph Igali <jaigali1@sheffield.ac.uk>
2016-12-06 00:24:01 +0000

    Implemented a splash page

--- floodit.rb
+++ floodit.rb
@@ -1,3 +1,30 @@
+require 'console_splash'
+require 'colorize'
+splash = ConsoleSplash.new(30,80)
+splash.write_header("Flood-It", "Joseph Igali", "0.0.1")
+splash.write_top_pattern(">")
+splash.write_bottom_pattern("<")
+splash.write_vertical_pattern("*")
+'clear'
+splash.splash
+userInput = gets.chomp
+def menu 
+  puts "Main Menu:"
+  puts "s = start game"
+  puts "c = change size"
+  puts "q = quit"
+  if (highscore == nil) then 
+    puts "No games played yet"
+  else 
+      highscore
+  end
+  userChoice = "Please enter your choice"
+end
+if (userInput == "\n") then 
+  menu
+end
+
+    
 
 def get_board(width, height)
   # TODO: Implement this method
